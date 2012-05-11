3e2c91089f0f07b368aac4dc01b4dc00fb516e8d
chaosymphony@gmail.com <chaosymphony@gmail.com@cf7fa615-fc65-96b6-b069-c816662ac64e>
2012-05-11 14:48:21 +0000

    implementing data load and DB insert, select.

--- trunk/SampleCode/HelloDaumMap/src/com/hello/daumMap/GeoPointLoader.java
+++ trunk/SampleCode/HelloDaumMap/src/com/hello/daumMap/GeoPointLoader.java
@@ -10,23 +10,82 @@ import android.content.res.Resources;
 
 public class GeoPointLoader {
 
+	public static final int LOAD_TYPE_COURSE_AND_STAMP = 0;
+	public static final int LOAD_TYPE_COURSE_COORDS = 1;
+	public static final int LOAD_TYPE_STAMP_COORDS = 2;
+	
 	public static final int ALL_COURSE_COUNT = 17;
 	public static final int COURSE_COORDS_START_INDEX = 2130968576;
+	public static final int STAMP_COORDS_START_INDEX = 2130968610;
 	
 	private final Context mCtx;
 	
 	List<List<MapPoint>> allCourseCoords = new ArrayList<List<MapPoint>>();
+	List<List<MapPoint>> stampCoords = new ArrayList<List<MapPoint>>();
 	List<List<MapPoint>> dividedCourseCoords = new ArrayList<List<MapPoint>>();
 	
-	public GeoPointLoader(Context ctx) {
+	public GeoPointLoader(Context ctx, int loadDataType) {
 		mCtx = ctx;
 
-		loadCourseCoords();
+		if(LOAD_TYPE_COURSE_AND_STAMP == loadDataType) {
+			loadCoords();
+		} else if(LOAD_TYPE_COURSE_COORDS == loadDataType) {
+			loadCourseCoords();
+		} else if(LOAD_TYPE_STAMP_COORDS == loadDataType) {
+			loadStampCoords();
+		}
 	}
 	
+	/**
+	 * 불필요한 메모리 낭비를 막기위해 모든 List 객체들의 clean 메소드 실행.
+	 */
+	public void cleanMyInventory() {
+		allCourseCoords.clear();
+		stampCoords.clear();
+		dividedCourseCoords.clear();
+	}
+	
+	
+	
+	/**
+	 * 모든 올레 코스 좌표 + 모든 스탬프 위치 좌표를 한번에 로드.
+	 * @return
+	 */
+	private boolean loadCoords() {
+		Resources res = mCtx.getResources();
+		int courseResIndex = COURSE_COORDS_START_INDEX;
+		int stampResIndex = STAMP_COORDS_START_INDEX;
+
+		for(int i = 0; i < ALL_COURSE_COUNT; i++) {
+			///////////////////////////////////////////////////////////ALL COURSE COORDS LOADING
+			String[] course_lat = res.getStringArray(courseResIndex++);
+			String[] course_lng = res.getStringArray(courseResIndex++);
+			int coords_count = course_lat.length;
+
+			List<MapPoint> tempCourseList = new ArrayList<MapPoint>();
+			for(int j = 0; j < coords_count; j++) {
+				tempCourseList.add(MapPoint.mapPointWithGeoCoord(Double.parseDouble(course_lat[j]), Double.parseDouble(course_lng[j])));
+			}
+			allCourseCoords.add(tempCourseList);
+
+			///////////////////////////////////////////////////////////STAMP COORDS LOADING
+			course_lat = res.getStringArray(stampResIndex++);
+			course_lng = res.getStringArray(stampResIndex++);
+			List<MapPoint> tempStampList = new ArrayList<MapPoint>();
+			for(int k = 0; k < 3; k++) {
+				tempStampList.add(MapPoint.mapPointWithGeoCoord(Double.parseDouble(course_lat[k]), Double.parseDouble(course_lng[k])));
+			}
+			stampCoords.add(tempStampList);
+		}
+		return true;
+	}
+	
+	/**
+	 * 모든 올레 코스 좌표를 로드.
+	 * @return
+	 */
 	private boolean loadCourseCoords() {
 		Resources res = mCtx.getResources();
-		
 		int resourceIndex = COURSE_COORDS_START_INDEX;
 		
 		for(int i = 0; i < ALL_COURSE_COUNT; i++) {
@@ -40,12 +99,36 @@ public class GeoPointLoader {
 			}
 			allCourseCoords.add(tempList);
 		}
+		return true;
+	}
+	
+	/**
+	 * 모든 스탬프 위치 좌표를 로드.
+	 * @return
+	 */
+	private boolean loadStampCoords() {
+		Resources res = mCtx.getResources();
+		int resourceIndex = STAMP_COORDS_START_INDEX;
 		
+		for(int i = 0; i < ALL_COURSE_COUNT; i++) {
+			String[] course_lat = res.getStringArray(resourceIndex++);
+			String[] course_lng = res.getStringArray(resourceIndex++);
+
+			List<MapPoint> tempList = new ArrayList<MapPoint>();
+			for(int j = 0; j < 3; j++) {
+				tempList.add(MapPoint.mapPointWithGeoCoord(Double.parseDouble(course_lat[j]), Double.parseDouble(course_lng[j])));
+			}
+			stampCoords.add(tempList);
+		}
 		return true;
 	}
 	
 	public List<MapPoint> getCourseGeopoints(int courseNo) {
-		return allCourseCoords.get(courseNo);
+		return allCourseCoords.get(--courseNo);
+	}
+	
+	public List<MapPoint> getStampGeopoints(int courseNo) {
+		return stampCoords.get(--courseNo);
 	}
 	
 	
